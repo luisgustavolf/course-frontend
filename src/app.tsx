7e42a2ffafb23ddfa48158e23e09803a381e9940
Luis Gustavo <luisgustavolf@gmail.com>
2023-06-14 19:59:52 -0300

    adicionado dependencias

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import { CssBaseline } from '@mui/material';
+import React from 'react';
+
+export function App() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+    </React.Fragment>
+  );
+}
+
